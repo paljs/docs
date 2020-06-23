58968cc89aa87d05e0132e58863ac834080f9683
Ahmed Elewa <ahmed2game@gmail.com>
2020-06-23 18:42:21 +0200

    update logo

--- gatsby-config.js
+++ gatsby-config.js
@@ -77,7 +77,7 @@ module.exports = {
         short_name: 'Pal',
         start_url: '/dashboard',
         display: 'minimal-ui',
-        icon: 'src/images/plack.png', // This path is relative to the root of the site.
+        icon: 'src/images/logo.png', // This path is relative to the root of the site.
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
